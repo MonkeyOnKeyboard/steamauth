943dc7d57256f25d728270d8296e96f7d43940d4
MonkeyOnKeyboard <markuskrueger84@yahoo.de>
2020-07-02 07:13:31 +0200

    Update config.php
    Change PHP-Version to 7.0

--- config/config.php
+++ config/config.php
@@ -25,7 +25,7 @@ class Config extends \Ilch\Config\Install
             ],
         ],
         'ilchCore' => '2.1.34',
-        'phpVersion' => '5.6'
+        'phpVersion' => '7.0'
     ];
 
     public function install()
